64b6de5ed1334457b23af93db2d026386b2b8fd0
Dom-Henrique <joaohenriqueds24@gmail.com>
2026-09-19 07:55:01 -0300

    Verification if already exists in DB.

--- src/main/java/com/dom/bean_lifecycle_demo/repository/PedidoRepository.java
+++ src/main/java/com/dom/bean_lifecycle_demo/repository/PedidoRepository.java
@@ -4,4 +4,5 @@ import com.dom.bean_lifecycle_demo.model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    boolean existsByNomePedido(String nomePedido);
 }
